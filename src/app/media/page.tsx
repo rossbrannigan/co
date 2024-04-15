905daec0c966258ce6304ecadf8a24a71b59db55
Ross Brannigan 2018 <rossbrannigan2018@Rosss-MacBook-Pro-2.local>
2024-04-15 23:49:17 +0700

    Updated Media 5

--- src/app/media/page.tsx
+++ src/app/media/page.tsx
@@ -139,7 +139,7 @@ const MediaPage = () => {
   </div>
   {/* Instagram Embed */}
   <div className="relative" style={{ maxWidth: "540px", margin: "0 auto" }}>
-    <blockquote className="instagram-media" data-instgrm-captioned data-instgrm-permalink="https://www.instagram.com/p/C4E_f_vp0yd/?utm_source=ig_embed&amp;utm_campaign=loading" data-instgrm-version="14" style={{ background: "#FFF", border: "0", borderRadius: "3px", boxShadow: "0 0 1px 0 rgba(0,0,0,0.5), 0 1px 10px 0 rgba(0,0,0,0.15)", margin: "1px", maxWidth: "540px", minWidth: "326px", padding: "0", width: "99.375%", WebkitCalc: "(100% - 2px)", calc: "(100% - 2px)" }}>
+    <blockquote className="instagram-media" data-instgrm-captioned data-instgrm-permalink="https://www.instagram.com/p/C4E_f_vp0yd/?utm_source=ig_embed&amp;utm_campaign=loading" data-instgrm-version="14" style={{ background: "#FFF", border: "0", borderRadius: "3px", boxShadow: "0 0 1px 0 rgba(0,0,0,0.5), 0 1px 10px 0 rgba(0,0,0,0.15)", margin: "1px", maxWidth: "540px", minWidth: "326px", padding: "0", width: "calc(99.375% - 2px)", calc: "calc(100% - 2px)" }}>
       {/* Instagram Embedded Content */}
       <div style={{ padding: "16px" }}>
         <a href="https://www.instagram.com/p/C4E_f_vp0yd/?utm_source=ig_embed&amp;utm_campaign=loading" style={{ background: "#FFFFFF", lineHeight: "0", padding: "0 0", textAlign: "center", textDecoration: "none", width: "100%" }} target="_blank">
@@ -174,22 +174,21 @@ const MediaPage = () => {
             </div>
             <div style={{ marginLeft: "8px" }}>
               <div style={{ backgroundColor: "#F4F4F4", borderRadius: "50%", flexGrow: "0", height: "20px", width: "20px" }}></div>
-              <div style={{ width: "0", height: "0", borderTop: "2px solid transparent", borderLeft: "6px solid #f4f4f4", borderBottom: "2px solid transparent", transform: "translateX(16px) translateY(-4px) rotate(30deg)" }}></div>
+              <div style={{ width: "0", height: "0", borderTop: "2px solid transparent", borderLeft: "6px solid #f4f4f4", borderBottom: "2px solid transparent", transform: "translateY(-4px) translateX(4px) rotate(30deg)", flexGrow: "0" }}></div>
+            </div>
+            <div style={{ marginRight: "8px", marginLeft: "4px" }}>
+              <div style={{ backgroundColor: "#F4F4F4", borderRadius: "50%", flexGrow: "0", height: "20px", width: "20px" }}></div>
+              <div style={{ width: "0", height: "0", borderTop: "2px solid transparent", borderLeft: "6px solid #f4f4f4", borderBottom: "2px solid transparent", transform: "translateY(-4px) translateX(4px) rotate(30deg)", flexGrow: "0" }}></div>
             </div>
             <div style={{ marginLeft: "auto" }}>
-              <div style={{ width: "0px", borderTop: "8px solid #F4F4F4", borderRight: "8px solid transparent", transform: "translateY(16px)" }}></div>
-              <div style={{ backgroundColor: "#F4F4F4", flexGrow: "0", height: "12px", width: "16px", transform: "translateY(-4px)" }}></div>
-              <div style={{ width: "0", height: "0", borderTop: "8px solid #F4F4F4", borderLeft: "8px solid transparent", transform: "translateY(-4px) translateX(8px)" }}></div>
+              <div style={{ backgroundColor: "#F4F4F4", borderRadius: "50%", height: "20px", width: "20px" }}></div>
+              <div style={{ width: "0", height: "0", border: "6px solid transparent", borderTopColor: "#FFF", borderBottom: "0", transform: "translateX(16px) translateY(-4px)" }}></div>
             </div>
           </div>
         </a>
-        <p style={{ color: "#c9c8cd", fontFamily: "Arial, sans-serif", fontSize: "14px", lineHeight: "17px", marginBottom: "0", marginTop: "8px", overflow: "hidden", padding: "8px 0 7px", textAlign: "center", textOverflow: "ellipsis", whiteSpace: "nowrap" }}>
-          <a href="https://www.instagram.com/p/C4E_f_vp0yd/?utm_source=ig_embed&amp;utm_campaign=loading" style={{ color: "#c9c8cd", fontFamily: "Arial, sans-serif", fontSize: "14px", fontStyle: "normal", fontWeight: "normal", lineHeight: "17px", textDecoration: "none" }} target="_blank">A post shared by Ross Brannigan (@rossbrannigan)</a>
-        </p>
       </div>
     </blockquote>
   </div>
-  {/* End Instagram Embed */}
 </main>
 
 
