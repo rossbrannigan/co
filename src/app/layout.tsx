375dcf370b9ddf7b8dfe354ba6e761e25a31adec
Ross Brannigan 2018 <rossbrannigan2018@Rosss-MacBook-Pro-2.local>
2024-04-16 16:53:42 +0700

    Roll Back

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { GoogleTagManager, GoogleAnalytics } from '@next/third-parties/google';
-import { useEffect } from 'react';
-import mixpanel from 'mixpanel';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,21 +9,12 @@ export const metadata: Metadata = {
   title: "Ross Brannigan | Leading Digital Business Results & Innovation",
   description: "Digital leader obsessed with commercial results, product innovation & customer satisfaction | IE MBA | APAC | ex-Etihad, ex-Airberlin, ex-Facebook",
 };
-
+{/* Important for Google Analytics to work - visit https://nextjs.org/docs/messages/next-script-for-ga */}
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
-  useEffect(() => {
-    // Initialize Mixpanel
-    mixpanel.init("ec0d1ce4d06ee41c0de80f1869b50426"); // Replace with your Mixpanel project token
-
-    // Track page view
-    mixpanel.track("Page View");
-  }, []);
-
   return (
     <html lang="en">
       <body className={inter.className}>
@@ -36,3 +25,4 @@ export default function RootLayout({
     </html>
   );
 }
+
